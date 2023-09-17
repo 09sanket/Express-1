b15b555c77715d47d84c8ebe43a8800a995c2617
sanket rahangdale <sanketrahangdale1118@gmail.com>
2023-09-17 14:36:53 +0530

    second commit

--- public/about.js
+++ public/about.js
@@ -6,4 +6,18 @@ const publicPath = path.join(__dirname , 'public');
 
 // app.use(express.static(publicPath)); commenting because do nit use this during removing urls 
 
+app.get('' , (req , resp)=>{
+    resp.sendFile(`${publicPath}/home.html`)
+});
+
+
+app.get('/about' , (req , resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+});
+
+
+app.get('*' , (req , resp)=>{
+    resp.sendFile(`${publicPath}/errorpage.html`)
+});
+
 app.listen(5000);
