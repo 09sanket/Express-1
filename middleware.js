e35b81df8221d6cd343bcaad5371b6c2accda795
sanket rahangdale <sanketrahangdale1118@gmail.com>
2023-09-17 15:42:21 +0530

    routers

---
+++ middleware.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+
+//  middleware filter 
+const reqFilter = (req , resp , next) =>{
+    if(!req.query.age){
+        resp.send("please provide age ");
+    }else if(req.query.age<18){
+        resp.send("you can not acces this page ")
+    }else{
+        next();
+    }
+    
+}
+
+app.use(reqFilter)
+
+
+app.get('/' , (req, resp) =>{
+    resp.send('welcome to home page')
+});
+
+
+app.get('/users' , (req, resp) =>{
+    resp.send('welcome to Users page')
+});
+
+
+app.get('/about' , (req, resp) =>{
+    resp.send('welcome to about page')
+});
+
+app.listen(5000);
